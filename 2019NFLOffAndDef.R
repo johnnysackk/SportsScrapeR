e849ee589d2737962f33d349259363a9ac3bf478
johnnysackk <64500255+johnnysackk@users.noreply.github.com>
2020-04-29 20:43:56 -0400

    Create 2019NFLOffAndDef.R

---
+++ 2019NFLOffAndDef.R
@@ -0,0 +1,36 @@
+library(rvest)
+
+x <- 1
+
+teamList <- list('crd', 'atl', 'rav', 'buf', 'car', 'chi', 'cin', 'cle', 'dal', 'den', 'det', 'gnb', 'htx', 'clt', 'jax', 'kan', 'sdg', 'ram', 'mia', 'min', 'nwe', 'nor', 'nyg', 'nyj', 'rai', 'phi', 'pit', 'sfo', 'sea', 'tam', 'oti', 'was')
+
+team <- teamList[[x]]
+
+setwd("C:/Users/johnny.burke/Documents/R/2019NFLOffense")
+
+offenseLog <- function(team) {
+  url <- read_html(paste0("https://www.pro-football-reference.com/teams/", team, "/2019/gamelog/"))
+  file <- paste0("offense", team, "2019.csv")
+  table <- url %>%
+    html_nodes("table") %>%
+    .[1] %>%
+    html_table(fill = TRUE)
+  write.csv(table, file=file)
+}
+
+lapply(teamList, offenseLog)
+
+setwd("C:/Users/johnny.burke/Documents/R/2019NFLDefense")
+
+defenseLog <- function(team) {
+  url <- read_html(paste0("https://www.pro-football-reference.com/teams/", team, "/2019/gamelog/"))
+  file <- paste0("defense", team, "2019.csv")
+  table <- url %>%
+    html_nodes("table") %>%
+    .[2] %>%
+    html_table(fill = TRUE)
+  write.csv(table, file=file)
+}
+
+
+lapply(teamList, defenseLog)
